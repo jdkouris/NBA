d05cedcc85ca6d3a5faaa2705b9d4af2a2a9568b
John Kouris <jdkouris@gmail.com>
2019-10-07 13:29:27 -0700

    Set up the storyboard and added the PlayersTableViewController and APIController files

---
+++ NBA/Application/APIController.swift
@@ -0,0 +1,15 @@
+//
+//  APIController.swift
+//  NBA
+//
+//  Created by John Kouris on 10/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class APIController {
+    
+    
+    
+}
---
+++ NBA/View Controllers/PlayersTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlayersTableViewController.swift
+//  NBA
+//
+//  Created by John Kouris on 10/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PlayersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
