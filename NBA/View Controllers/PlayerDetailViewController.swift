6b91128fef3b3be01f6793d3f731d36e2f5d7328
John Kouris <jdkouris@gmail.com>
2019-10-08 09:28:06 -0700

    Created the player detail view and added the PlayerDetailViewController with outlets

---
+++ NBA/View Controllers/PlayerDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlayerDetailViewController.swift
+//  NBA
+//
+//  Created by John Kouris on 10/8/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailViewController: UIViewController {
+    
+    @IBOutlet var lastNameLabel: UILabel!
+    @IBOutlet var firstNameLabel: UILabel!
+    @IBOutlet var positionLabel: UILabel!
+    @IBOutlet var teamNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
